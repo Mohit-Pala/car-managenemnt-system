4a8f52b68abeae437ca31ee18a7112a690cdc688
Mohit Pala <palav@appstate.edu>
2024-11-16 23:13:42 -0500

    updated to use local server

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,18 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { firebaseConfig } from '../../config/firebase_config';
+import { connectDataConnectEmulator, getDataConnect } from 'firebase/data-connect';
+import { connectorConfig } from './car-generated';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
   provideRouter(routes),
-  provideFirebaseApp(() => initializeApp(firebaseConfig)),
+  provideFirebaseApp(() => {
+    const app = initializeApp(firebaseConfig);
+    // Need to initialize Data Connect with connectorConfig
+    const dataConnect = getDataConnect(app, connectorConfig);
+    connectDataConnectEmulator(dataConnect, "localhost", 9399);
+    return app;
+  }),
   provideAuth(() => getAuth())]
 };
