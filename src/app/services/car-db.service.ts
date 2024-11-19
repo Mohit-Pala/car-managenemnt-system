3531dc49a0ba43441a0e4121b126b0d2f75663d1
Mohit Pala <palav@appstate.edu>
2024-11-18 21:26:31 -0500

    update service

--- src/app/services/car-db.service.ts
+++ src/app/services/car-db.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { listCarById, ListCarByIdData, listCars, ListCarsData } from '../car-generated';
+import { createCar, CreateCarData, deleteCar, listCarById, ListCarByIdData, listCars, ListCarsData } from '../car-generated';
 
 
 @Injectable({
@@ -27,7 +27,7 @@ export const handleListCarByID = async (
 
 export const handleListAllCars = async (
 
-) : Promise<ListCarsData["cars"] | null> => {
+): Promise<ListCarsData["cars"] | null> => {
   try {
     const response = await listCars({});
     if (response.data.cars) {
@@ -36,6 +36,73 @@ export const handleListAllCars = async (
     return null;
   } catch (error) {
     console.error("Error fetching cars:", error);
+    return null;
+  }
+};
+
+export const handleDeleteCarById = async (
+  id: string
+): Promise<void> => {
+  try {
+    const response = await deleteCar({ id: id }); // Assuming deleteCarById is imported
+  } catch (error) {
+    console.error("Error deleting car:", error);
+  }
+};
+
+
+import { updateCar, UpdateCarData } from '../car-generated'; // Import necessary types
+
+export const handleUpdateCar = async (
+  id: string,
+  make: string,
+  model: string,
+  color: string,
+  releaseYear: number,
+  electric: boolean
+): Promise<UpdateCarData["car_update"] | null> => {
+  try {
+    const response = await updateCar({
+      id: id,
+      make: make,
+      model: model,
+      color: color,
+      releaseYear: releaseYear,
+      electric: electric
+    });
+    if (response.data.car_update) {
+      return response.data.car_update;
+    }
+    return null;
+  } catch (error) {
+    console.error("Error updating car:", error);
+    return null;
+  }
+};
+
+export const handleAddCar = async (
+  id: string,
+  make: string,
+  model: string,
+  color: string,
+  releaseYear: number,
+  electric: boolean
+): Promise<CreateCarData["car_insert"] | null> => {
+  try {
+    const response = await createCar({
+      id: id,
+      make: make,
+      model: model,
+      color: color,
+      releaseYear: releaseYear,
+      electric: electric
+    });
+    if (response.data.car_insert) {
+      return response.data.car_insert;
+    }
+    return null;
+  } catch (error) {
+    console.error("Error inserting car:", error);
     return null;
   }
 };
